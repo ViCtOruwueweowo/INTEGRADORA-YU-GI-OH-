f0c8b8a08c01661cf5b3f7a387fd69f40dc80cb9
RickGrimmes <babajasgo@hotmail.com>
2023-08-01 13:13:04 -0600

    consulta de modificar dc corregida

--- views/administrador/funciones/modificar_dc.php
+++ views/administrador/funciones/modificar_dc.php
@@ -128,10 +128,14 @@ $conexion = new database();
 $conexion->conectarDB();
 
 // Obtener la lista de departamentos para el filtro
-$consulta = "SELECT CONCAT('Cliente:', ' ',clientes.nom_cli,'. ', 'Carta:',' ', cartas.nombre_c, ' ', rareza.rareza) as nombre, deuda_c.id_dc, deuda_c.precio_c,
- deuda_c.cantidad_c, deuda_c.precio_c, deuda_c.notas, deuda_c.abono_c,car_rar.cantidad FROM deuda_c INNER JOIN clientes ON deuda_c.id_clientec = clientes.id_cli 
- INNER JOIN car_rar ON car_rar.id_cr = deuda_c.cr_fk inner join cartas on car_rar.id_carar=cartas.id_car inner join rareza on car_rar.id_rar=rareza.id_ra  GROUP BY nombre";
+//$consulta = "SELECT CONCAT('Cliente:', ' ',clientes.nom_cli,'. ', 'Carta:',' ', cartas.nombre_c, ' ', rareza.rareza) as nombre, deuda_c.id_dc, deuda_c.precio_c, deuda_c.cantidad_c, deuda_c.precio_c, deuda_c.notas, deuda_c.abono_c,car_rar.cantidad FROM deuda_c INNER JOIN clientes ON deuda_c.id_clientec = clientes.id_cli INNER JOIN car_rar ON car_rar.id_cr = deuda_c.cr_fk inner join cartas on car_rar.id_carar=cartas.id_car inner join rareza on car_rar.id_rar=rareza.id_ra GROUP BY nombre";
+//$tabla = $conexion->seleccionar($consulta);
+
+// Obtener la lista de departamentos para el filtro
+$consulta = "SELECT CONCAT('Cliente:', ' ',clientes.nom_cli,'. ', 'Carta:',' ', cartas.nombre_c, ' ', rareza.rareza) as nombre, deuda_c.id_dc, deuda_c.precio_c, deuda_c.cantidad_c, deuda_c.precio_c, deuda_c.notas, deuda_c.abono_c, deuda_c.estado_c, deuda_c.concepto ,car_rar.cantidad FROM deuda_c INNER JOIN clientes ON deuda_c.id_clientec = clientes.id_cli INNER JOIN car_rar ON car_rar.id_cr = deuda_c.cr_fk inner join cartas on car_rar.id_carar=cartas.id_car inner join rareza on car_rar.id_rar=rareza.id_ra WHERE deuda_c.estado_c='ACTIVO' ORDER BY clientes.nom_cli ASC";
 $tabla = $conexion->seleccionar($consulta);
+
+
 
 // Filtrar el departamento seleccionado
 if (isset($_POST['depa'])) {
