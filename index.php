8ea3ddba2de073043125eccc47921ebd0459ee09
Victor_99 <107272608+ViCtOruwueweowo@users.noreply.github.com>
2023-08-03 22:09:06 -0600

    hola

--- index.php
+++ index.php
@@ -206,7 +206,7 @@ $db->desconectarDB();
       <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-dots" viewBox="0 0 16 16">
   <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
   <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg> 871-33-44-172</a></p>
+</svg> 871 103 4946</a></p>
     </div>
   </div>
 
@@ -217,7 +217,7 @@ $db->desconectarDB();
 <div class="container text-center">
 <span class="text-center" style="color:white">Aplicacion Desarrollada Unicamente Para Fines De Venta Y Distribucion De Menores.</span><br>
 <span class="text-center" style="color:white">Plaza de la Tecnología Torreón, Av Hidalgo 1334, Primitivo Centro, 27000 Torreón, Coah. Segundo piso Local #288</span><br>
-<span class="text-center  fw-bold" style="color:white">Cel. 871-33-44-172</span>
+<span class="text-center  fw-bold" style="color:white">Cel. 871 103 4946</span>
 </div>
 </footer>
 </body>
