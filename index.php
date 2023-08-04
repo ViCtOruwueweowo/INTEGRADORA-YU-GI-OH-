c8d21fce2f1ef76100a99e23989409fafebdfae0
Victor_99 <107272608+ViCtOruwueweowo@users.noreply.github.com>
2023-08-04 07:41:45 -0600

    U//w//U

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="js/bootstrap.bundle.min.js"></script>
   <script src="js/bootstrap.bundle.js"></script>
+  <link rel="stylesheet" href="css/whatssap.css">
   <script src="js/bootstrap.min.js"></script>
 </head>
 <body bgcolor="#ededed">
@@ -206,7 +207,11 @@ $db->desconectarDB();
       <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-dots" viewBox="0 0 16 16">
   <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
   <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg> 871 103 4946</a></p>
+</svg> 871 103 4946</a>  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=8711034946&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+  </p>
     </div>
   </div>
 
